2a393b7ccfa8f4c06e58e03466852fda11193d61
KanishkaKodithuwakku <infomail.kbk@gmail.com>
2023-08-14 22:18:46 +0530

    todos validations added

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const Button = ({btnText, handleOnClick }) => {
+const Button = ({btnText, handleOnClick,disabled }) => {
   return (
     <>
       <button
         className="btn btn-primary mt-2 mb-2 btn-sm"
         onClick={handleOnClick}
+        disabled={disabled}
       >
         {btnText}
       </button>
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,29 +1,50 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { TodoContext } from "../context/TodoContext";
 import List from "./List";
 import TextInput from "./TextInput";
 import Button from "./Button";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 const Todos = () => {
-  const { todo, setTodo,todos, setTodos } = useContext(TodoContext);
+  const { todo, setTodo, todos, setTodos } = useContext(TodoContext);
+  const [disabled, setDisabled] = useState(true)
+
+  // if (todo) { 
+  //   setDisabled(false)
+  // }
 
   function handleOnChange(value) {
-    setTodo([value]);
+    setTodo(value);
+    setDisabled(false);
   }
 
   function handleOnClick() {
+    if (todos.includes(todo)) { 
+      toast.warn(todo+ " is already added")
+       setTodo("");
+      return
+    }
+     
     setTodos([...todos, todo]);
     setTodo("");
+    setDisabled(true);
   }
   return (
     <>
       <div className="row">
         <div className="col-sm-6">
+          <ToastContainer/>
           <TextInput
             value={todo}
             handleOnChange={(e) => handleOnChange(e.target.value)}
           />
-          <Button handleOnClick={handleOnClick} btnText={`ADD`} />
+          <Button
+            handleOnClick={handleOnClick}
+            btnText={`ADD`}
+            disabled={disabled}
+          />
         </div>
       </div>
       <List />
